b07222acbfba3ecb522588b9704fb729e3d9c304
tongfeima <43868938+tongfeima@users.noreply.github.com>
2020-07-19 08:49:05 +0800

    Update main.c

--- Robust/main.c
+++ Robust/main.c
@@ -13,7 +13,7 @@ int main(void)
 		map[1][3]=malloc(sizeof(GraphNode)); map[1][3]->ceil=7;map[1][3]->floor=3;
 		map[1][4]=malloc(sizeof(GraphNode)); map[1][4]->ceil=5;map[1][4]->floor=2;
 		map[2][3]=malloc(sizeof(GraphNode)); map[2][3]->ceil=7;map[2][3]->floor=3;
-		map[2][4]=malloc(sizeof(GraphNode)); map[0][2]->ceil=6;map[0][2]->floor=2;
+		map[2][4]=malloc(sizeof(GraphNode)); map[2][4]->ceil=6;map[2][4]->floor=2;
 		map[3][4]=malloc(sizeof(GraphNode)); map[3][4]->ceil=4;map[3][4]->floor=2;
 		map[3][5]=malloc(sizeof(GraphNode)); map[3][5]->ceil=6;map[3][5]->floor=4;
 		map[4][5]=malloc(sizeof(GraphNode)); map[4][5]->ceil=7;map[4][5]->floor=3;
@@ -22,8 +22,7 @@ int main(void)
 	QUEUE Q;
 	InitializeTable(T);
 	Q=InitializeQueue();
-	RobustCalculate(map,0,T,Q);
-	//ShowTable(T);
+	Robust(map,T,Q);
+
 	return 0;	
 }	
-
